37d046ed5e637624ba6b57a85f7785890890adfb
TheStrix <parthbhatia98@gmail.com>
2015-09-09 14:49:38 +0000

    lettuce: Add hardware info
    Change-Id: Idcb60b250f20290ef09bd24bdbf32649e92ba46c

---
+++ product/aicp-hwprop.mk
@@ -0,0 +1,8 @@
+# Hardware Info
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.chipset=Qualcomm Snapdragon 410 MSM8916 \
+    ro.device.cpu=1.2GHz Quad-core Cortex-A53 \
+    ro.device.gpu=Qualcomm Adreno 306 \
+    ro.device.rear_cam=8 MP \
+    ro.device.front_cam=5 MP \
+    ro.device.screen_res=720x1280
