ab12de775c19044f375798afe10a9941bec1a68d
TheStrix <parthbhatia98@gmail.com>
2016-01-14 04:00:11 -0500

    lettuce: Add libhealthd.msm library
    Few APIs are provided in the libhealthd library as the supplement of
    off-mode charging, it includes:
     1) Draw battery status on the animation;
     2) Handle charging disabled;
    
    - Removed led related stuffs as lettuce do not have leds
    - Switch to standard libhealthd.msm from CAF
    Change-Id: Ifa42ccec70e4f3e7b20fda5ce3f33dc41252a440
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.usb.id.ums_adb=3008 \
     ro.usb.vid=2a96
 
+# libhealthd library as the supplement of off-mode charging
+PRODUCT_PACKAGES += \
+    libhealthd.msm
+
 # Doze mode
 PRODUCT_PACKAGES += \
     YUDoze
@@ -81,10 +85,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
-# Healthd to boot directly from charger mode rather than initiating a reboot.
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.enable_boot_charger_mode=1
 
 # Keylayout
 PRODUCT_COPY_FILES += \
