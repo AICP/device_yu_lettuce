423d18a5a8e7401e34860a5c928d348f11122f5b
Pranav Vashi <neobuddy89@gmail.com>
2016-06-05 14:12:17 +0530

    lettuce: Add vibrator cmhw interface
    Change-Id: I3ee15fc4baf6666652d178d86f5544b86522669c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,3 +1,5 @@
+# CMHW
+BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
 # Tap to wake
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/double_tap_enable"
 TARGET_USE_SDCLANG := true
