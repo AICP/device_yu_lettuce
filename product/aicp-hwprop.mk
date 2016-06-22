5272c793214d5e8ebccfdac0dbb980863e9d5085
TheStrix <parthbhatia98@gmail.com>
2016-06-22 10:10:17 -0400

    lettuce: Remove hw info props
    Change-Id: Iad0fce3049116a715efdd49e5c59192afa7f11b4

--- product/aicp-hwprop.mk
+++
@@ -1,8 +0,0 @@
-# Hardware Info
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.device.chipset="Qualcomm Snapdragon 410 MSM8916" \
-    ro.device.cpu="1.2GHz quad-core Cortex-A53" \
-    ro.device.gpu="Qualcomm Adreno 306" \
-    ro.device.rear_cam="8 MP 3264x2448 pixels" \
-    ro.device.front_cam="5 MP" \
-    ro.device.screen_res="720x1280"
